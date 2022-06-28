9f9fead5d8644e2284c5581ebb503371741f1984
Protochuu <chat.et.coq@gmail.com>
2022-06-28 13:32:44 -0400

    nuevos cambios

--- JuanGrafoysuPandilla.cpp
+++ JuanGrafoysuPandilla.cpp
@@ -4,16 +4,16 @@ using namespace std;
 class Graph{
  
 vector<vector<int>> adjList;
-
+int n; 
 Graph(int n){
     vector<int> list;
+    this->n = n; 
     for(int i = 0; i < n; i++){
         adjList.push_back(list);
     }
 }
 void addEdge(int u, int v){
-    
-
+    adjList[u].push_back(v);
 }
 bool checkConnectivityDFS(int u){
 
